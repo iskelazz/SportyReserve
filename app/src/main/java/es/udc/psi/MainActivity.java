6cb73bca4c5b3960f77d8c3024ccc84cf04366e2
Alex <a.riverag@udc.es>
2023-04-03 21:36:43 +0200

    Firebase enabled

--- app/src/main/java/es/udc/psi/MainActivity.java
+++ app/src/main/java/es/udc/psi/MainActivity.java
@@ -1,6 +1,8 @@
 package es.udc.psi;
 
 import androidx.appcompat.app.AppCompatActivity;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import android.os.Bundle;
 
@@ -10,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
     }
 }
