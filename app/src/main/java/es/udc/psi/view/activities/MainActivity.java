98296d49a113bc05da99e43e48be3c56a1108525
Alex <a.riverag@udc.es>
2023-05-12 21:29:18 +0200

    Fix small errors

--- app/src/main/java/es/udc/psi/view/activities/MainActivity.java
+++ app/src/main/java/es/udc/psi/view/activities/MainActivity.java
@@ -1,12 +1,15 @@
 package es.udc.psi.view.activities;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
@@ -105,13 +108,13 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             case R.id.nav_about:
                 // Lanza la actividad de "acerca de"
                 // Reemplaza AcercaDeActivity.class con la clase de actividad adecuada
-                intent = new Intent(this, RegisterActivity.class);
-                startActivity(intent);
+                about();
                 break;
             case R.id.nav_logout:
                 //intent = new Intent(this, RegisterActivity.class);
                 //startActivity(intent);
-                about();
+                intent = new Intent(this, LoginActivity.class);
+                startActivity(intent);
                 break;
         }
 
