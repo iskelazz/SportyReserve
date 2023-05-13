12b5b2d34c8cfd852675ad4e2c415a91731697a3
Alex <a.riverag@udc.es>
2023-05-13 17:11:55 +0200

    Permite a los usuarios ver su nombre y mail en el menuLateral

--- app/src/main/java/es/udc/psi/repository/interfaces/UserRepository.java
+++ app/src/main/java/es/udc/psi/repository/interfaces/UserRepository.java
@@ -6,8 +6,9 @@ import com.google.firebase.auth.AuthResult;
 import es.udc.psi.model.User;
 
 public interface UserRepository {
+    String getCurrentUserId();
     void createUser(User usuario, OnUserCreatedListener listener);
-
+    void getUser(String uid, final OnUserFetchedListener listener);
     void checkUsernameExists(String username, OnUsernameCheckedListener listener);
 
     void signInWithEmailAndPassword(String email, String password, OnCompleteListener<AuthResult> listener);
@@ -21,5 +22,9 @@ public interface UserRepository {
         void onExists();
         void onNotExists();
         void onFailure(String errorMessage);
+    }
+    interface OnUserFetchedListener {
+        void onFetched(User user);
+        void onFailure(String error);
     }
 }
