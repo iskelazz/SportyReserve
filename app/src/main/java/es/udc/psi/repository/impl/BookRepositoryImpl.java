4172ae4d9ba7755682269956685ac2916d6c2385
Javier <Javi520@users.noreply.github.com>
2023-05-21 20:09:37 +0200

    Lista de usuarios + correciones varias
    - Ahora ya se añade una lista de usuarios, a la hora de hacer reserva, a la reserva. (Siempre es una lista con el anfitrión)
    - Ya no se usa calendar en la clase "Reserve".
    - Ahora las reservas se indexan con el mismo ID que tienen como campo, en Firebase

--- app/src/main/java/es/udc/psi/repository/impl/BookRepositoryImpl.java
+++ app/src/main/java/es/udc/psi/repository/impl/BookRepositoryImpl.java
@@ -39,7 +39,7 @@ public class BookRepositoryImpl implements BookRepository {
         // TODO Tener los datos del usuario logeado en algún sitio para no tener que pedirlo
         //      y poder hacer cosas como la anterior
         //book.setPlayerList(new ArrayList<User>().add(null));
-        mDatabase.push().setValue(book)
+        mDatabase.child(book.getId()).setValue(book)
                 .addOnSuccessListener(aVoid -> listener.onSuccess())
                 .addOnFailureListener(e -> listener.onFailure(e.getMessage()));
     }
